e08e05223f606e46a2b108be11625363f93cc2a8
Jprtc <jprtc8@gmail.com>
2023-04-15 04:36:49 -0300

    Fixed build errors and optimized images with the <Image/> tag from next

--- src/Components/BlogPostPreview/index.tsx
+++ src/Components/BlogPostPreview/index.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 interface BlogPostPreviewProps {
   slug: string;
@@ -7,10 +8,21 @@ interface BlogPostPreviewProps {
   coverImageUrl: string;
 }
 
-const BlogPostPreview = ({ slug, title, excerpt, coverImageUrl }: BlogPostPreviewProps) => {
+const BlogPostPreview = ({
+  slug,
+  title,
+  excerpt,
+  coverImageUrl,
+}: BlogPostPreviewProps) => {
   return (
     <div className="bg-white rounded-lg shadow-sm p-6 mb-8">
-      <img src={coverImageUrl} alt={title} className="w-full h-48 object-cover rounded mb-4" />
+      <Image
+        src={coverImageUrl}
+        alt={title}
+        width={800}
+        height={400}
+        className="w-full h-48 object-cover rounded mb-4"
+      />
       <h2 className="text-2xl font-semibold text-gray-800 mb-2">{title}</h2>
       <p className="text-gray-600 mb-4">{excerpt}</p>
       <Link href={`/blog/${slug}`} passHref legacyBehavior>
--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,37 +1,43 @@
-import { useRouter } from 'next/router';
-import Navbar from '@/Components/Navbar';
-import Link from 'next/link';
+import { useRouter } from "next/router";
+import Navbar from "@/Components/Navbar";
+import Link from "next/link";
+import Image from "next/image";
 
 const BlogPost = () => {
-    const router = useRouter();
-    const { slug } = router.query;
-  
-    return (
-      <div className="min-h-screen bg-gray-100">
-        <Navbar />
-        <main className="container mx-auto px-4 mt-10">
-          <h1 className="text-4xl mb-4 text-gray-800">Blog Post: {slug}</h1>
-          <img src="https://via.placeholder.com/800x400" alt="Cover image" className="w-full h-auto object-cover rounded mb-4" />
-          <div className="bg-white p-4 rounded">
-            <p className="text-gray-600 leading-relaxed">
-              Content for blog post "{slug}" goes here. You can add as much text as you want to describe the content of the blog post.
-              Feel free to use any formatting, such as <strong className="text-gray-800">bold</strong>, <em className="text-gray-800 italic">italic</em>, or even <u>underline</u> to make the content
-              more engaging. Remember to keep your paragraphs concise and informative.
-            </p>
-            <div className="mt-8">
-              <Link href="/" passHref legacyBehavior>
-                <a className="text-green-600 font-semibold">← Back to home</a>
-              </Link>
-            </div>
+  const router = useRouter();
+  const { slug } = router.query;
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <main className="container mx-auto px-4 mt-10">
+        <h1 className="text-4xl mb-4 text-gray-800">Blog Post: {slug}</h1>
+        <Image
+          src="https://via.placeholder.com/800x400"
+          alt="Cover image"
+          className="w-full h-auto object-cover rounded mb-4"
+          width={800}
+          height={400}
+        />
+        <div className="bg-white p-4 rounded">
+          <p className="text-gray-600 leading-relaxed">
+            Content for blog post {slug} goes here. You can add as much text as
+            you want to describe the content of the blog post. Feel free to use
+            any formatting, such as{" "}
+            <strong className="text-gray-800">bold</strong>,{" "}
+            <em className="text-gray-800 italic">italic</em>, or even{" "}
+            <u>underline</u> to make the content more engaging. Remember to keep
+            your paragraphs concise and informative.
+          </p>
+          <div className="mt-8">
+            <Link href="/" passHref legacyBehavior>
+              <a className="text-green-600 font-semibold">← Back to home</a>
+            </Link>
           </div>
-        </main>
-      </div>
-    );
-  };
-  
-  export default BlogPost;
-  
-  
-  
-  
-  
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default BlogPost;
